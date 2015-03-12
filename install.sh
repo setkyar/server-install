cf7e3d5ea3d0861d723b470a9d103a51131646f5
setkyar <setkyar16@gmail.com>
2015-03-12 10:32:05 +0630

    Add missing

--- install.sh
+++ install.sh
@@ -5,6 +5,12 @@ echo " --- Hello Baddy! --- "
 echo " --- Update packages Lists --- "
 sudo apt-get update
 
+echo " --- Remove apache2 via server --- "
+sudo apt-get purge apache2*
+
+echo " --- Re updating packages --- "
+sudo apt-get update
+
 echo " --- Installing Base Packages --- "
 sudo apt-get install -y unzip vim git-core curl wget build-essential python-software-properties
 
@@ -13,15 +19,16 @@ sudo add-apt-repository -y ppa:ondrej/php5
 sudo apt-get update
 sudo apt-get install -y php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt mysql-server-5.5 php5-mysql git-core
 
-echo "-- It's Time For MySQL Baddy --"
-sudo apt-get install -y mysql-server libapache2-mod-auth-mysql php5-mysql
-
 echo " --- It's Time For Nginx Baddy --- "
 sudo add-apt-repository -y ppa:nginx/stable
 sudo apt-get update
 sudo apt-get install -y nginx
 
+echo "-- It's Time For MySQL Baddy --"
+sudo apt-get install -y mysql-server libapache2-mod-auth-mysql php5-mysql
+
 echo " --- It's Time For HHVM Baddy --- "
+sudo apt-get update
 wget -O - http://dl.hhvm.com/conf/hhvm.gpg.key | sudo apt-key add -
 echo deb http://dl.hhvm.com/ubuntu trusty main | sudo tee /etc/apt/sources.list.d/hhvm.list
 sudo apt-get update
@@ -32,7 +39,11 @@ sudo /usr/share/hhvm/install_fastcgi.sh
 sudo update-rc.d hhvm defaults
 sudo service hhvm restart
 
+echo " --- Enable HHVM to cli --"
+sudo /usr/bin/update-alternatives --install /usr/bin/php php /usr/bin/hhvm 60
+
 echo " --- It's time for Composer Baddy --- "
+sudo apt-get update
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 
@@ -40,4 +51,4 @@ echo "-- Fixing ResourceLimit Timeout for composer --"
 ResourceLimit.SocketDefaultTimeout=30 # in seconds  
 Http.SlowQueryThreshold=30000
 
-echo " --- Everything is done Baddy --- "
+echo " --- I had installed Nginx, MySQL, HHVM, Composer --- "
